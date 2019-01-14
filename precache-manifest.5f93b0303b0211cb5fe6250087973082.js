681afb540c692c99e88bcb85df5d2a873cee0766
Hemesh Reddy <Hemesh.Reddy@cheproximity.com.au>
2019-01-15 09:54:01 +1100

    Updates

---
+++ precache-manifest.5f93b0303b0211cb5fe6250087973082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0824cab5052d3161c112",
+    "url": "/numerology-app/static/js/runtime~main.0824cab5.js"
+  },
+  {
+    "revision": "55319d7051674e9fa47a",
+    "url": "/numerology-app/static/js/main.55319d70.chunk.js"
+  },
+  {
+    "revision": "2cfd82824bb64cef857d",
+    "url": "/numerology-app/static/js/1.2cfd8282.chunk.js"
+  },
+  {
+    "revision": "55319d7051674e9fa47a",
+    "url": "/numerology-app/static/css/main.70b2536b.chunk.css"
+  },
+  {
+    "revision": "77a4cd5bcc3723b84907997e721e0289",
+    "url": "/numerology-app/index.html"
+  }
+];
