bb8522edba143d66223cde7fd64691a49eb9e606
Hemesh Reddy <Hemesh.Reddy@cheproximity.com.au>
2019-01-17 16:04:40 +1100

    Updates

--- precache-manifest.964b6905a79aa0bec894bf905b37744d.js
+++ precache-manifest.964b6905a79aa0bec894bf905b37744d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/numerology-app/static/js/runtime~main.0824cab5.js"
   },
   {
-    "revision": "918077fbb631ee4f8124",
-    "url": "/numerology-app/static/js/main.918077fb.chunk.js"
+    "revision": "71c8372840a55a5cdeec",
+    "url": "/numerology-app/static/js/main.71c83728.chunk.js"
   },
   {
     "revision": "e63aab64fdc5637cc63c",
     "url": "/numerology-app/static/js/1.e63aab64.chunk.js"
   },
   {
-    "revision": "918077fbb631ee4f8124",
-    "url": "/numerology-app/static/css/main.5290e230.chunk.css"
+    "revision": "71c8372840a55a5cdeec",
+    "url": "/numerology-app/static/css/main.616d5f2d.chunk.css"
   },
   {
-    "revision": "1b7ad27d0d1ba292bbaea1bb746de0cc",
+    "revision": "74b36ae80d93ed4161ef8ff347a0cb69",
     "url": "/numerology-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/numerology-app/precache-manifest.9feaf9bec6c186705a77d5240e5778f7.js"
+  "/numerology-app/precache-manifest.964b6905a79aa0bec894bf905b37744d.js"
 );
 
 workbox.clientsClaim();
